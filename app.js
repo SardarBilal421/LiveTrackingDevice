bfd75d7499aeb1fd8df589f547a5ddbdaa66b478
Sardar Bilal <rockeykhan142@gmail.com>
2024-07-23 20:02:00 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+const path = require("path");
+
+const app = express();
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+io.on("connection", function (socket) {
+  socket.on("send-location", function (data) {
+    io.emit("recieve-location", { id: socket.id, ...data });
+  });
+});
+
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+server.listen(3000);
